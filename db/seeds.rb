024cae603bc8446c52f0d37f68c53ecd3b00ae1b
Alexander Giraldo <alexander@hlmbusiness.com>
2012-07-23 22:39:02 -0500

    Load fake posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: 'Hello', body: 'world')
+Post.create(title: 'foo', body: 'bar')
